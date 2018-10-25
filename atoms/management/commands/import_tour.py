d55c70cccfb7ba509d0ef48253e498e28f6b9b97
Miguel Vieira <jmvieira@gmail.com>
2018-10-25 12:36:27 +0100

    The command takes a new argument, the title of the collection the documents being imported need to be added to.

--- atoms/management/commands/import_tour.py
+++ atoms/management/commands/import_tour.py
@@ -3,17 +3,23 @@ import os
 import re
 from json import JSONDecodeError
 
-from django.core.management.base import BaseCommand
 from atoms.models import Location, Media, Source, Text
+from django.core.management.base import BaseCommand
+from themes.models import Collection
 
 
 class Command(BaseCommand):
     help = 'Imports data from the Portus tour into the database'
 
     def add_arguments(self, parser):
+        parser.add_argument('collection', nargs=1, type=str)
         parser.add_argument('tour_path', nargs='+', type=str)
 
     def handle(self, *args, **options):
+        collection_title = options['collection']
+        collection, _ = Collection.objects.get_or_create(
+            title=collection_title)
+
         for tour_path in options['tour_path']:
             if not os.path.isfile(tour_path):
                 self.stderr.write('Invalid file path: {}'.format(tour_path))
@@ -24,19 +30,24 @@ class Command(BaseCommand):
                     tour = json.load(f)
 
                 self.stdout.write('Importing tour file: {}'.format(tour_path))
-                self.import_tour(os.path.basename(tour_path), tour)
+                self.import_tour(collection, os.path.basename(tour_path), tour)
             except JSONDecodeError as e:
                 self.stderr.write(
                     'Failed to load the json file at: {}'. format(tour_path))
                 continue
 
-    def import_tour(self, title, data):
+        # updates the term data for the collection
+        collection.save()
+
+    def import_tour(self, collection, title, data):
         source = self.get_or_create_source('tour: {}'.format(title))
 
         for slide in data['storymap']['slides']:
-            text = self.get_or_create_text(source, slide['text'])
-            media = self.get_or_create_media(source, slide['media'])
-            location = self.get_or_create_location(source, slide['location'])
+            text = self.get_or_create_text(collection, source, slide['text'])
+            media = self.get_or_create_media(
+                collection, source, slide['media'])
+            location = self.get_or_create_location(
+                collection, source, slide['location'])
 
             if media:
                 text.items.add(media)
@@ -58,15 +69,16 @@ class Command(BaseCommand):
         source, _ = Source.objects.get_or_create(title=title)
         return source
 
-    def get_or_create_text(self, source, data):
+    def get_or_create_text(self, collection, source, data):
         text, _ = Text.objects.get_or_create(
             source=source, title=data['headline'])
+        text.collection = collection
         text.content = data['text']
         text.save()
 
         return text
 
-    def get_or_create_media(self, source, data):
+    def get_or_create_media(self, collection, source, data):
         if not data['url']:
             return None
 
@@ -80,6 +92,7 @@ class Command(BaseCommand):
 
         media, _ = Media.objects.get_or_create(
             source=source, title=title, url=url)
+        media.collection = collection
         media.credit = data['credit']
         media.save()
 
@@ -99,7 +112,7 @@ class Command(BaseCommand):
 
         return url
 
-    def get_or_create_location(self, source, data):
+    def get_or_create_location(self, collection, source, data):
         lat = data['lat']
         lon = data['lon']
 
@@ -110,6 +123,7 @@ class Command(BaseCommand):
 
         location, _ = Location.objects.get_or_create(
             source=source, title=title, lat=lat, lon=lon)
+        location.collection = collection
         location.zoom = data['zoom']
         location.save()
 
